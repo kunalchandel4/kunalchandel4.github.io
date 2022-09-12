a5c0338128eb78db614fb7968926e9cf1ff38701
kunal chandel <kunalchandel266@gmail.com>
2022-09-13 00:22:52 +0530

    sructure

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ $(document).ready(function(){
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active")
-       
+        $('.menu-btn i').toggleClass("active")
     }) ;    
 })
