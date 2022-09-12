10b235434251e41d2fc4eafbc5a3e4348f52e174
kunal chandel <kunalchandel266@gmail.com>
2022-09-13 02:14:20 +0530

    add carousel

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 
+  //   toogel caste thing menu
 
-$(document).ready(function(){
-         
-      $(window).scroll(function(){
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-                if(this.scrollY >20){
-                   
-                     $('.navbar').addClass("sticky") ;
-                }else{
-                    $('.navbar').removeClass("sticky") 
-                   
-                }
+  // owl carousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
 
-      })
+    responsive: {
+      0: {
+        item: 1,
+        nav: false,
+      },
+      600: {
+        item: 2,
+        nav: false,
+      },
 
-
-    //   toogel caste thing menu
-
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active")
-        $('.menu-btn i').toggleClass("active")
-    }) ;    
-})
+      1000: {
+        item: 3,
+        nav: false,
+      },
+    },
+  });
+});
