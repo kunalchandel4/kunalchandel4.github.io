a3b5b724c7f6b21ac2bb49115d52beac09bd7bc0
kunal chandel <kunalchandel266@gmail.com>
2022-09-13 12:44:33 +0530

    project

--- script.js
+++ script.js
@@ -16,25 +16,25 @@ $(document).ready(function () {
 
   // owl carousel
   $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
+    // margin: 20,
+    // loop: true,
+    // autoplayTimeOut: 2000,
+    // autoplayHoverPause: true,
 
-    responsive: {
-      0: {
-        item: 1,
-        nav: false,
-      },
-      600: {
-        item: 2,
-        nav: false,
-      },
+    // responsive: {
+    //   0: {
+    //     item: 1,
+    //     nav: false,
+    //   },
+    //   600: {
+    //     item: 2,
+    //     nav: false,
+    //   },
 
-      1000: {
-        item: 3,
-        nav: false,
-      },
-    },
+    //   1000: {
+    //     item: 3,
+    //     nav: false,
+    //   },
+    //},
   });
 });
